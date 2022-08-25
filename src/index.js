9c9265c54662852cc8e907ab1a5d3cf7af1a2684
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-25 12:25:37 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    toDoList(e.target['new-task-description'].value);=
+
+  })
+ 
 });
+
+
+function toDoList(task){
+  let ul = document.querySelector('#tasks')
+  let li = document.createElement('li');
+  li.textContent = `${task}`;
+
+
+//add delete button
+let btn = document.createElement('button');
+btn.textContent = "y"
+ul.appendChild(btn);
+ul.appendChild(li);
+
+
+btn.addEventListener('click', (e) => {
+  e.targer.parentNode.remove();
+})
+}
